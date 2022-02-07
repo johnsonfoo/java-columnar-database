41878b7ed21864629a1588cb47675983a38008ef
Johnson Foo <37036242+johnsonfoo@users.noreply.github.com>
2022-02-07 19:44:48 +0800

    GH-1 - Create CsvFileManager class

--- src/main/java/com/ntu/bdm/ColumnarDatabaseApplication.java
+++ src/main/java/com/ntu/bdm/ColumnarDatabaseApplication.java
@@ -2,7 +2,11 @@ package com.ntu.bdm;
 
 public class ColumnarDatabaseApplication {
 
-  public static void main(String[] args) {
-    System.out.println("Hello World");
+  public static void main(String[] args) throws Exception {
+    CsvFileManager csvFileManager = new CsvFileManager();
+    csvFileManager.readAll("SingaporeWeather.csv");
+    csvFileManager.print();
+    csvFileManager.clear();
+    csvFileManager.print();
   }
 }
