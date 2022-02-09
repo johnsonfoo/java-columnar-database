c6bc3ae39b730c9f1b8e3c35684d9059b87eaa37
Johnson Foo <37036242+johnsonfoo@users.noreply.github.com>
2022-02-09 16:24:10 +0800

    GH-5 - Create AbstractColumnVector class

---
+++ src/main/java/com/ntu/bdm/vector/AbstractColumnVector.java
@@ -0,0 +1,98 @@
+package com.ntu.bdm.vector;
+
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.List;
+
+public abstract class AbstractColumnVector<T> implements ColumnVector<T> {
+
+  private int valueCount;
+  private int nullCount;
+  private BitSet validityVector;
+  private List<T> dataVector;
+
+  public AbstractColumnVector() {
+    valueCount = 0;
+    nullCount = 0;
+    validityVector = new BitSet();
+    dataVector = new ArrayList<>();
+  }
+
+  /**
+   * Gets the number of values.
+   *
+   * @return number of values in the vector
+   */
+  @Override
+  public int getValueCount() {
+    return valueCount;
+  }
+
+  /**
+   * Returns number of null elements in the vector.
+   *
+   * @return number of null elements
+   */
+  @Override
+  public int getNullCount() {
+    return nullCount;
+  }
+
+  /**
+   * Get the element at the given index from the data vector.
+   *
+   * @param index position of element
+   * @return element at given index
+   */
+  @Override
+  public T get(int index) {
+    return dataVector.get(index);
+  }
+
+  /**
+   * Check whether an element in the vector is null.
+   *
+   * @param index index to check for null
+   * @return true if element is null
+   */
+  @Override
+  public boolean isNull(int index) {
+    return !validityVector.get(index);
+  }
+
+  /**
+   * Gets the validity vector.
+   *
+   * @return vector of bits tracking which elements in the vector are null
+   */
+  @Override
+  public BitSet getValidityVector() {
+    return validityVector;
+  }
+
+  /**
+   * Gets the data vector.
+   *
+   * @return vector of data
+   */
+  @Override
+  public List<T> getDataVector() {
+    return dataVector;
+  }
+
+  /**
+   * Insert value into the data vector and increment number of values in vector.
+   * <p>
+   * If value is not null, flip bit representing index of value in data vector to 1 in validity
+   * vector. Else, increment number of null elements in vector.
+   */
+  public void add(T value) {
+    if (value != null) {
+      validityVector.set(valueCount);
+    } else {
+      nullCount++;
+    }
+    dataVector.add(value);
+    valueCount++;
+  }
+}
