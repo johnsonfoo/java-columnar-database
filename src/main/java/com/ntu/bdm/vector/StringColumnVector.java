899450d1a347eb4ee5181c605616af543c4df221
Johnson Foo <37036242+johnsonfoo@users.noreply.github.com>
2022-02-09 18:17:23 +0800

    GH-11 - Create CategoricalStringColumnVector class

--- src/main/java/com/ntu/bdm/vector/StringColumnVector.java
+++ src/main/java/com/ntu/bdm/vector/StringColumnVector.java
@@ -1,25 +1,5 @@
 package com.ntu.bdm.vector;
 
-import java.util.HashSet;
-
 public class StringColumnVector extends AbstractColumnVector<String> {
 
-  private HashSet<String> uniqueData;
-
-  public StringColumnVector() {
-    super();
-    uniqueData = new HashSet<>();
-  }
-
-  public HashSet<String> getUniqueData() {
-    return uniqueData;
-  }
-
-  @Override
-  public void add(String value) {
-    if (value != null) {
-      uniqueData.add(value);
-    }
-    super.add(value);
-  }
 }
