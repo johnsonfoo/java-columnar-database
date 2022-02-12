203276a90f2fc5996b6527aa1bc1e6f6fa7a49ce
johnsonfoo <johnsonfoo18@gmail.com>
2022-02-12 17:05:41 +0800

    GH-31 - Add populateColumnVectorsFromCsv method

--- src/main/java/com/ntu/bdm/ColumnarDatabaseApplication.java
+++ src/main/java/com/ntu/bdm/ColumnarDatabaseApplication.java
@@ -1,15 +1,16 @@
 package com.ntu.bdm;
 
+import com.ntu.bdm.util.DateUtility;
+import java.util.List;
+
 public class ColumnarDatabaseApplication {
 
   public static void main(String[] args) throws Exception {
     CsvFileManager csvFileManager = new CsvFileManager();
     csvFileManager.readAll("SingaporeWeather.csv");
-    csvFileManager.print();
-    csvFileManager.clear();
-    csvFileManager.print();
 
     ColumnVectorManager columnVectorManager = createColumnVectorsFromCsv();
+    populateColumnVectorsFromCsv(csvFileManager, columnVectorManager);
   }
 
   public static ColumnVectorManager createColumnVectorsFromCsv() {
@@ -23,5 +24,32 @@ public class ColumnarDatabaseApplication {
     columnVectorManager.createDoubleColumnVector("Humidity");
 
     return columnVectorManager;
+  }
+
+  public static void populateColumnVectorsFromCsv(CsvFileManager csvFileManager,
+      ColumnVectorManager columnVectorManager) {
+    List<List<String>> csvRows = csvFileManager.getCsvRows();
+
+    for (int i = 1; i < csvRows.size(); i++) {
+      String timestamp = csvRows.get(i).get(1);
+      columnVectorManager.addToStringColumnVector("Timestamp", timestamp);
+
+      String year = DateUtility.parseAndGetYear(timestamp);
+      columnVectorManager.addToCategoricalColumnVector("Year", year);
+
+      String month = DateUtility.parseAndGetMonth(timestamp);
+      columnVectorManager.addToCategoricalColumnVector("Month", month);
+
+      String station = csvRows.get(i).get(2);
+      columnVectorManager.addToCategoricalColumnVector("Station", station);
+
+      String temperatureString = csvRows.get(i).get(3);
+      Double temperature = temperatureString.equals("M") ? null : Double.valueOf(temperatureString);
+      columnVectorManager.addToDoubleColumnVector("Temperature", temperature);
+
+      String humidityString = csvRows.get(i).get(4);
+      Double humidity = humidityString.equals("M") ? null : Double.valueOf(humidityString);
+      columnVectorManager.addToDoubleColumnVector("Humidity", humidity);
+    }
   }
 }
