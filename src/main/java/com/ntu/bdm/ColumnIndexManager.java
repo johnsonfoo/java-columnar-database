befa0b1b565bb44fe960771c64efb8d787a5ac07
johnsonfoo <johnsonfoo18@gmail.com>
2022-02-12 18:53:22 +0800

    GH-33 - Add constructCategoricalColumnIndexes method

--- src/main/java/com/ntu/bdm/ColumnIndexManager.java
+++ src/main/java/com/ntu/bdm/ColumnIndexManager.java
@@ -3,10 +3,11 @@ package com.ntu.bdm;
 import com.ntu.bdm.index.CategoricalColumnIndex;
 import com.ntu.bdm.vector.CategoricalColumnVector;
 import java.util.HashMap;
+import java.util.Map;
 
 public class ColumnIndexManager {
 
-  private HashMap<String, CategoricalColumnIndex<String>> categoricalColumnIndexes;
+  private Map<String, CategoricalColumnIndex<String>> categoricalColumnIndexes;
 
   public ColumnIndexManager() {
     categoricalColumnIndexes = new HashMap<>();
@@ -18,7 +19,16 @@ public class ColumnIndexManager {
     categoricalColumnIndexes.get(fieldName).constructBitmapIndex(categoricalColumnVector);
   }
 
-  public HashMap<String, CategoricalColumnIndex<String>> getCategoricalColumnIndexes() {
+  public void constructCategoricalColumnIndexes(
+      Map<String, CategoricalColumnVector<String>> categoricalColumnVectors) {
+    for (Map.Entry<String, CategoricalColumnVector<String>> entry : categoricalColumnVectors.entrySet()) {
+      String fieldName = entry.getKey();
+      CategoricalColumnVector<String> categoricalColumnVector = entry.getValue();
+      constructCategoricalColumnIndex(fieldName, categoricalColumnVector);
+    }
+  }
+
+  public Map<String, CategoricalColumnIndex<String>> getCategoricalColumnIndexes() {
     return categoricalColumnIndexes;
   }
 }
