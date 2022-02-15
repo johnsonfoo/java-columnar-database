e8570a0504d076819e850e89017da8de97cc7ee3
Johnson Foo <37036242+johnsonfoo@users.noreply.github.com>
2022-02-15 16:50:25 +0800

    GH-51 - Add writeDataLineByLine method

--- src/main/java/com/ntu/bdm/CsvFileManager.java
+++ src/main/java/com/ntu/bdm/CsvFileManager.java
@@ -1,7 +1,11 @@
 package com.ntu.bdm;
 
+import com.opencsv.CSVWriter;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -28,6 +32,33 @@ public class CsvFileManager {
     }
     System.out.println("CSV file read to memory");
   }
+
+  public void writeDataLineByLine(String filePath, String[] data) {
+
+    // first create file object for file placed at location
+    // specified by filepath
+    File file = new File(filePath);
+
+    try {
+      // create FileWriter object with file as parameter
+      FileWriter outputFile = new FileWriter(file, true);
+
+      // create CSVWriter with ',' as separator
+      CSVWriter writer = new CSVWriter(outputFile, ',',
+          CSVWriter.NO_QUOTE_CHARACTER,
+          CSVWriter.DEFAULT_ESCAPE_CHARACTER,
+          CSVWriter.DEFAULT_LINE_END);
+
+      // add data to csv
+      writer.writeNext(data);
+
+      // closing writer connection
+      writer.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
 
   public void clear() {
     csvRows.clear();
